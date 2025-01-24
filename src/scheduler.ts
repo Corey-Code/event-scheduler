5ed6288209631d5fb4fde3a5255db6c2694a7c9a
Corey-Code <37006206+Corey-Code@users.noreply.github.com>
2025-01-24 09:59:31 -0500

    wip scheduleJob

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,8 +1,35 @@
 import cron from 'node-cron';
 import Job from './models/job';
+import { DateTime } from 'luxon';
 
 const scheduleJob = (job: any) => {
-  // TODO:
+  // Run the job if the start date is less than or equal to current time
+  if (DateTime.fromISO(job.startDate).toMillis() <= DateTime.now().toMillis()) {
+    // Example Event Structure –
+    // {
+    //     “eventName”: ”Morning Scene”,
+    //     “startDate”: ”2024-01-01”,
+    //     “endDate”: ”2024-01-31”,
+    //     “recurrence”: {
+    //         “interval”:”1”
+    //         “frequency”: ”day”
+    //     }
+    // }
+
+    // DEBUG job's parameters to console structure later
+    console.log(`Running Job: ${job.eventName}`);
+    console.log(`Start Date: ${job.startDate}`);
+    console.log(`End Date: ${job.endDate}`);
+    console.log(
+      `Recurrence Interval: ${job.recurrence.interval} ${job.recurrence.frequency}`
+    );
+
+    // Update job status in DB
+    Job.findByIdAndUpdate(job._id, { status: 'completed' }).exec();
+  } else {
+    // If the job hasn't reached the start date, skip scheduling it now
+    console.log(`Skipping Job: ${job.eventName} (not yet started)`);
+  }
 };
 
 const runScheduledJobs = async () => {
@@ -13,8 +40,8 @@ const runScheduledJobs = async () => {
     jobs.forEach((job) => {
       // Schedule the job with a cron expression
       cron.schedule('*/1 * * * *', () => {
-        console.log('[DEBUG] (scheduler.ts) runScheduledJobs -job', job);
-        // scheduleJob(job);
+        // console.log('[DEBUG] (scheduler.ts) runScheduledJobs -job', job);
+        scheduleJob(job);
       });
     });
   } catch (error) {
